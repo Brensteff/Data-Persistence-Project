584e65b9e9bb69b3cb78cf95d436ad240fa587cd
Brensteff <BRENDENRILEY@OUTLOOK.COM>
2021-11-10 01:39:09 -0500

    General Fixes

--- Data-Persistence-Starter-Files/Assets/Scripts/MainManager.cs
+++ Data-Persistence-Starter-Files/Assets/Scripts/MainManager.cs
@@ -24,7 +24,7 @@ public class MainManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        MenuHandler.InputField("playerName");
+        //MenuHandler.InputField("playerName");
         const float step = 0.6f;
         int perLine = Mathf.FloorToInt(4.0f / step);
         LoadScore();
